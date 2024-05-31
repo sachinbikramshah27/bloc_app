df0003a6eb9f97492d7828740c75cfdf89b9b7b5
sachinbikramshah27 <obitokurama2754@gmail.com>
2024-05-31 11:27:49 +0545

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(390, 844),
+      designSize: const Size(390, 840),
       minTextAdapt: true,
       splitScreenMode: true,
       useInheritedMediaQuery: true,
